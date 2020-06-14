d16de04d7e8053617befea30b82847602490a546
Tetiana <tatka777t@gmail.com>
2020-06-14 20:24:35 +0300

    Updates

---
+++ precache-manifest.e2c0b295ffea5b15958d749795ef679b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1ba27c177bd4d646f84e1645730cf8",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "9e826cdbf089d83c301c",
+    "url": "/goit-react-hw-07-phonebook/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "e5e8d28b3f2827363a74",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.68395161.chunk.css"
+  },
+  {
+    "revision": "9e826cdbf089d83c301c",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.8e186516.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.8e186516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e8d28b3f2827363a74",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.7d588a10.chunk.js"
+  },
+  {
+    "revision": "b0ee86f873d795c0676a",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.8a222599.js"
+  }
+]);
